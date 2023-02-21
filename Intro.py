080247ca3d5ee5b0f02c3d40d1c52ee60c7812c4
garutmaan <klausurmusik@gmail.com>
2023-02-21 20:16:37 +0530

    Intro Change

--- Intro.py
+++ Intro.py
@@ -1,6 +1,6 @@
 import datetime
 
 print("Welcome to PYTHON Class")
-today_date = datetime.datetime.now() 
-# Sunday, 19th February at 04:59 PM
-print(f"Held on:{today_date.strftime('%A, %dth %B at %I:%M %p')}") 
+today_date = datetime.datetime.now()
+# Tuesday, 21st February at 04:59 PM
+print(f"Held on:{today_date.strftime('%A, %dth %B at %I:%M %p')}")
