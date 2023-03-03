a0db2304d42709ab05f821b7ffaf10d1182a9eb4
garutmaan <klausurmusik@gmail.com>
2023-03-03 11:30:30 +0530

    Project Setup

--- Intro.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-print("Welcome to PYTHON Class")
-today_date = datetime.datetime.now()
-# Tuesday, 21st February at 04:59 PM
-print(f"Held on:{today_date.strftime('%A, %dth %B at %I:%M %p')}")
