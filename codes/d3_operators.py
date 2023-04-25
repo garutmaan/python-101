5d1b96ebd25d74e51577053681df46dbde0c371a
garutmaan <klausurmusik@gmail.com>
2023-04-25 11:30:39 +0530

    feature-1008: fresh start-Day3-Operators-arithmetic

---
+++ codes/d3_operators.py
@@ -0,0 +1,25 @@
+"""
+Code: Day-3
+Concept : Operators and Operations
+Learner : Hanish
+Sub Topic : Arithmetic Operators
+"""
+
+# arithmetic operators
+number_1 = 100
+number_2 = 200
+
+result_value=number_1+number_2 # addition
+print(result_value)
+result_value=number_1-number_2 # minus
+print(result_value)
+result_value=number_1*number_2 # multiplication
+print(result_value)
+result_value=number_1/number_2 # decimal division
+print(result_value)
+result_value=number_1//number_2 # Floor Division
+print(result_value)
+result_value=number_1**number_2 # Exponent
+print(result_value)
+result_value=number_1%number_2 # Remainder, modulus
+print(result_value)
